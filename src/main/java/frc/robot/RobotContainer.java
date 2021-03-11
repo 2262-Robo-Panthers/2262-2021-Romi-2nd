86ad449d14aacedc65b88310f16838c2fd859123
Mukesh Khatri <xsword.mk@gmail.com>
2021-03-10 23:55:17 -0500

    Initial commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,109 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import frc.robot.commands.DriveCommand;
+import frc.robot.subsystems.RomiDrivetrain;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+	// The robot's subsystems and commands are defined here...
+	private final RomiDrivetrain m_romiDrivetrain = new RomiDrivetrain();
+
+	private final XboxController m_controller = new XboxController(0);
+
+	/**
+	 * The container for the robot. Contains subsystems, OI devices, and commands.
+	 */
+	public RobotContainer() {
+		// Configure the button bindings
+		configureButtonBindings();
+	}
+
+	/**
+	 * Use this to pass the autonomous command to the main {@link Robot} class.
+	 *
+	 * @return the command to run in autonomous
+	 */
+	public Command getAutonomousCommand() {
+
+		final DifferentialDriveKinematics driveKinematics =
+			new DifferentialDriveKinematics(Constants.kTrackwidthMeters);
+
+		final SimpleMotorFeedforward driveFeedforward =
+			new SimpleMotorFeedforward(Constants.ksVolts, Constants.kvVoltSecondsPerMeter, Constants.kaVoltSecondsSquaredPerMeter);
+
+		final String trajectoryJSON = "paths/Auto.wpilib.json";
+		Trajectory autoTrajectory = new Trajectory();
+		try {
+			final Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+			autoTrajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+		} catch (IOException ex) {
+			DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+		}
+
+		final RamseteCommand ramseteCommand = new RamseteCommand(
+			autoTrajectory,
+			m_romiDrivetrain::getPose,
+			new RamseteController(Constants.kRamseteB, Constants.kRamseteZeta),
+			driveFeedforward,
+			driveKinematics,
+			m_romiDrivetrain::getWheelSpeeds,
+			new PIDController(Constants.kPDriveVel, 0.0, 0.0),
+			new PIDController(Constants.kPDriveVel, 0.0, 0.0),
+			m_romiDrivetrain::tankDriveVolts,
+			m_romiDrivetrain
+		);
+
+		m_romiDrivetrain.resetOdometry(autoTrajectory.getInitialPose());
+
+		final Trajectory finalTrajectory = autoTrajectory;
+		return new InstantCommand(() -> m_romiDrivetrain.resetOdometry(finalTrajectory.getInitialPose()), m_romiDrivetrain)
+			.andThen(ramseteCommand)
+			.andThen(m_romiDrivetrain::stopDrivetrain, m_romiDrivetrain);
+	}
+
+	/**
+	 * Use this method to define your button->command mappings. Buttons can be
+	 * created by instantiating a {@link GenericHID} or one of its subclasses
+	 * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+	 * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+	 */
+	private void configureButtonBindings() {
+		m_romiDrivetrain.setDefaultCommand(
+			new InstantCommand(() -> m_romiDrivetrain.resetOdometry(Constants.kInitialPose), m_romiDrivetrain)
+			.andThen(
+				new DriveCommand(
+					m_romiDrivetrain,
+					() -> -m_controller.getY(Hand.kLeft),
+					() -> m_controller.getX(Hand.kRight),
+					() -> m_controller.getBumper(Hand.kRight)
+				)));
+	}
+
+}
